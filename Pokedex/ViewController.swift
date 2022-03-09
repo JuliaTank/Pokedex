805659579b88b0ae0f38f0583f903a9c2f77e1f8
JuliaTank <tankiewicz.julia@gmail.com>
2022-03-09 19:08:36 +0100

    started view

--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        //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
     }
 
 
